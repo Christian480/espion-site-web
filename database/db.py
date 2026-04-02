6c485448ccb580c87217e50a43d24f43e4c38b17
christian <Christian.teraou@gmail.com>
2026-04-03 01:06:28 +0200

    Ajout des dernières modifications

--- database/db.py
+++ database/db.py
@@ -164,67 +164,7 @@ def get_specialites(db_name="shadowcomm.db"):
     )
 
 
-<<<<<<< HEAD
-
-def init_db():
-    db = sqlite3.connect("shadowcomm.db")
-    cursor = db.cursor()
-
-    cursor.executescript("""
-    CREATE TABLE IF NOT EXISTS niveau (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nom TEXT NOT NULL
-    );
-
-    CREATE TABLE IF NOT EXISTS specialite (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        specialite TEXT NOT NULL
-    );
-
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        code_name TEXT NOT NULL UNIQUE,
-        niveau_id INTEGER NOT NULL,
-        specialite_id INTEGER NOT NULL,
-        password BLOB NOT NULL,
-        FOREIGN KEY (niveau_id) REFERENCES niveau(id),
-        FOREIGN KEY (specialite_id) REFERENCES specialite(id)
-    );
-                         
-    CREATE TABLE IF NOT EXISTS message ( 
-         id INTEGER PRIMARY KEY AUTOINCREMENT, 
-         sender_id INTEGER NOT NULL, 
-         content TEXT NOT NULL, 
-         timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
-         FOREIGN KEY (sender_id) REFERENCES users(id)
-     );
-
-    INSERT OR IGNORE INTO niveau (id, nom) VALUES (1,'S'), (2,'A'), (3,'C');
-
-    INSERT OR IGNORE INTO specialite (id, nom) VALUES
-    (1,'agent-double'),
-    (2,'agent-infiltré'),
-    (3,'agent-informateur'),
-    (4,'cyber-espion');
-    """)
-
-    cursor.execute("DROP TABLE IF EXISTS messages")
-
-    db.commit()
-    db.close()
-
-
-init_db()
-
-
-create_user("agent007", 1, 4, "agent007@2026")
-create_user("agentX", 2, 1, "motdepasse123")
-create_user("ghost", 3, 2, "secret456")
-
-print("Base de données Shadowcomm initialisée avec succès !")
-=======
 if __name__ == "__main__":
     init_db()
     seed_default_users()
     print("Base de données ShadowComm initialisée avec succès !")
->>>>>>> christian
--- services/caesar_cipher.py
+++ services/caesar_cipher.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-def caesar_encode(text, shift=3):
-    result = ""
-    for char in text:
-        if char.isalpha():
-            # On gère les majuscules (code ASCII 65-90)
-            start = ord('A') if char.isupper() else ord('a')
-            # Calcul du décalage circulaire
-            new_char = chr((ord(char) - start + shift) % 26 + start)
-            result += new_char
-        else:
-            result += char # On ne touche pas aux espaces/ponctuation
-    return result
-
-def caesar_decode(text, shift=3):
-    # Déchiffrer, c'est décaler dans l'autre sens
-    return caesar_encode(text, -shift)
-=======
 def encrypt_message(message, key=3):
     result = ""
 
@@ -36,4 +18,3 @@ def encrypt_message(message, key=3):
 
 def decrypt_message(message, key=3):
     return encrypt_message(message, -key)
->>>>>>> christian
